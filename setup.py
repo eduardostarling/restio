886aa9647ce04706aee5d962296e3310e5151777
Eduardo Starling <edmstar@gmail.com>
2019-07-31 15:57:26 +0200

    Reorganizing folders to comply with standard

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/eduardostarling/restio.git",
     packages=['restio'],
-    package_dir={'': 'src'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
