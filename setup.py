e3bd471a1af63f44cf6ea628265833a138210223
Eduardo Starling <edmstar@gmail.com>
2019-11-09 18:04:22 +0100

    Adding build steps to pipeline, adding publish to PyPI step

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
 import setuptools
+import restio
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+read_the_docs_url = "https://restio.readthedocs.io/en/latest"
+git_url = "https://github.com/eduardostarling/restio.git"
+
 setuptools.setup(
-    name="restio",
-    version="0.0.1",
+    name=restio.__name__,
+    version=restio.__version__,
     author="Eduardo Starling",
     author_email="edmstar@gmail.com",
-    description="Rest Integration Framework",
+    description="restio Framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/eduardostarling/restio.git",
+    url=git_url,
     packages=['restio'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
+    project_urls={
+        "Documentation": read_the_docs_url,
+        "Source Code": git_url,
+    }
 )
