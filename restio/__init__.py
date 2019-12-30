960acfca4aea9fb8be72edcf98e5c522dab0ed1a
Eduardo Starling <edmstar@gmail.com>
2019-12-30 19:39:58 +0100

    Bumping to version 0.1.0

--- restio/__init__.py
+++ restio/__init__.py
@@ -2,14 +2,14 @@ from .cache import ModelCache, QueryCache
 from .dao import BaseDAO
 from .event import EventListener
 from .graph import DependencyGraph, Node, Tree
-from .model import BaseModel, PrimaryKey
+from .model import BaseModel, PrimaryKey, mdataclass
 from .query import BaseQuery, query
 from .state import ModelState, ModelStateMachine, Transition
 from .transaction import (Transaction, TransactionError,
                           TransactionOperationError, TransactionState)
 
 __name__ = "restio"
-__version__ = "0.0.5"
+__version__ = "0.1.0"
 
 __all__ = [
     'BaseModel',
@@ -18,5 +18,7 @@ __all__ = [
     'Transaction',
     'ModelState',
     'PrimaryKey',
-    'query'
+    'query',
+    'mdataclass',
+    'TransactionError'
 ]
