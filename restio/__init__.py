44ea69f71e3eb33bda182105d5773c61150223df
Eduardo Starling <edmstar@gmail.com>
2019-12-31 00:22:52 +0100

    Bumping to version 0.1.1

--- restio/__init__.py
+++ restio/__init__.py
@@ -9,7 +9,7 @@ from .transaction import (Transaction, TransactionError,
                           TransactionOperationError, TransactionState)
 
 __name__ = "restio"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     'BaseModel',
