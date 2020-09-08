24c6d0d1a39513ded01c69d3d6d9d9719df7b11f
Eduardo Starling <edmstar@gmail.com>
2020-09-08 15:22:49 +0200

    chore: Update installer settings and documentation

--- setup.py
+++ setup.py
@@ -15,13 +15,16 @@ setuptools.setup(
     description="restio Framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=git_url,
     packages=["restio"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    project_urls={"Source Code": git_url},
+    url=git_url,
+    project_urls={
+        "Source Code": git_url,
+        "Documentation": "https://restio.readthedocs.io/en/latest/",
+    },
     python_requires=">=3.7",
 )
